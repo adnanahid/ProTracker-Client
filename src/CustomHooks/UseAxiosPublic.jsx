9d546bab967d7669805253ec7a05f364b5235f5d
adnanahid <sikuahamed@gmail.com>
2025-02-22 22:00:05 +0600

    check

--- src/CustomHooks/UseAxiosPublic.jsx
+++ src/CustomHooks/UseAxiosPublic.jsx
@@ -2,12 +2,12 @@ import { useMemo } from "react";
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  // baseURL: "https://a12-server-pi.vercel.app",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://a12-server-pi.vercel.app",
+  // baseURL: "http://localhost:5000",
 });
 
 const useAxiosPublic = () => {
   return axiosPublic;
 };
 
-export default useAxiosPublic;
+export default useAxiosPublic
